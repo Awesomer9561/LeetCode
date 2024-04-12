eb0befe93db9861f79a83b99c8462edca222dcf4
Parvez Ali <parvez@businesscode.app>
2024-04-13 01:12:31 +0530

    151. Reverse Words in a String

---
+++ ReverseWords.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    /// <summary>
+    /// https://leetcode.com/problems/product-of-array-except-self/?envType=study-plan-v2&envId=leetcode-75
+    /// </summary>
+    public partial class Solution
+    {
+        public string ReverseWords(string s)
+        {
+            var words = s.Trim().Split(' ');
+            var response = string.Join(" ", words.Where(word => !string.IsNullOrEmpty(word)).Reverse());
+
+            return response;
+        }
+    }
+}
