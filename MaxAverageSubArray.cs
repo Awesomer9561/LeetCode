a796d852362d607043fa727de2902d867722800b
Parvez Ali <parvez@businesscode.app>
2024-04-18 16:42:28 +0530

    643. Maximum Average Subarray I

---
+++ MaxAverageSubArray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    /// <summary>
+    /// https://leetcode.com/problems/maximum-average-subarray-i/?envType=study-plan-v2&envId=leetcode-75
+    /// </summary>
+    public partial class Solution
+    {
+        public double FindMaxAverage(int[] nums, int k)
+        {
+            double sum = 0, maxAvg;
+            for (int i = 0; i < k; i++)
+            {
+                sum += nums[i];
+            }
+            maxAvg = sum / k;
+
+            for (int i = k; i < nums.Length; i++)
+            {
+                sum += nums[i] - nums[i - k];
+                maxAvg = Math.Max(maxAvg, sum /k);
+            }
+
+            return maxAvg;
+        }
+    }
+}
